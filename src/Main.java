3cb44b0588a57759d588932400b3ac2021270c66
Miles Kurth <mkurth9612@student.pps.net>
2026-03-13 22:27:26 -0700

    working on it

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,41 @@
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionAdapter;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.PointerInfo;
 import javax.swing.*;
 
-public class Main {
+public class Main extends JFrame{
+    ActionListener taskPerformer = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            mX = (int) mousePos.getX();
+            mY = (int) mousePos.getY();
+        }
+    };
+    Timer timer = new Timer(50, taskPerformer);
+//    timer
+
     static Toolkit toolkit = Toolkit.getDefaultToolkit();
     static int screenWidth = toolkit.getScreenSize().width;
     static int screenHeight = toolkit.getScreenSize().height;
 
+    static Point mousePos = MouseInfo.getPointerInfo().getLocation();
     static int mX; static int mY;
 
     static JFrame window = new JFrame("You can't close me!");
-    static int windowWidth = 400;
-    static int windowHeight = 300;
+    static int windowWidth = 400; static int windowHeight = 300;
+    static int windowX = 500; static int windowY = 500;
 
     static JButton closeButton = new JButton();
 
     public static void main(String[] args) {
-        PointerInfo pointerInfo = MouseInfo.getPointerInfo();
-        Point mousePos = pointerInfo.getLocation();
         mX = (int) mousePos.getX();
         mY = (int) mousePos.getY();
 
+
         window.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-        window.setSize(400,300);
+        window.setBounds(windowX,windowY,windowWidth,windowHeight);
 
         window.addWindowListener(new WindowAdapter() {
             @Override
@@ -37,11 +47,16 @@ public class Main {
         window.addMouseMotionListener(new MouseMotionAdapter() {
             @Override
             public void mouseMoved(MouseEvent e) {
-                int winMX = e.getX();
-                int winMY = e.getY();
+                int winMX = e.getX() + windowX;
+                int winMY = e.getY() + windowY;
                 mX = (int) mousePos.getX();
                 mY = (int) mousePos.getY();
 
+                System.out.println(mX);
+//                System.out.println(windowX + ",  " + winMX + " " + winMY);
+                if (winMX >= windowX + 5 && winMX <= windowX + 22){
+                    System.out.println("aaaaaa");
+                }
 
             }
         });
@@ -49,7 +64,16 @@ public class Main {
         window.setVisible(true);
 
         while (true){
-
+            mX = (int) mousePos.getX();
+            mY = (int) mousePos.getY();
+            System.out.println(mX);
+//            mX = (int) mousePos.getX();
+//            mY = (int) mousePos.getY();
+//
+//            if (mX >= windowX && mX <= windowX + 80){
+//                System.out.println("aaaaaa");
+//                System.exit(0);
+//            }
         }
     }
 
@@ -61,6 +85,11 @@ public class Main {
         int newX = (int)(Math.random() * (screenWidth - windowWidth) );
         int newY = (int)(Math.random() * (screenHeight - windowHeight) );
         window.setBounds(newX,newY,windowWidth,windowHeight);
-//        window.setBounds(0,0,windowWidth,windowHeight);
+        windowX = window.getX();
+        windowY = window.getY();
     }
+
+
+
+
 }
