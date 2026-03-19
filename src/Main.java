019ea71d23c489305ac64aed3f392473716b7389
Miles-Kurth <mkurth9612@student.pps.net>
2026-03-18 17:29:18 -0700

    Next: beat task manager

--- src/Main.java
+++ src/Main.java
@@ -7,43 +7,66 @@ import java.util.Timer;
 import java.util.TimerTask;
 import javax.swing.*;
 
+
 public class Main extends JFrame{
 
-
+    //System
     static Toolkit toolkit = Toolkit.getDefaultToolkit();
     static int screenWidth = toolkit.getScreenSize().width;
     static int screenHeight = toolkit.getScreenSize().height;
 
-//    static Point mousePos = MouseInfo.getPointerInfo().getLocation();
+    static String operatingSystem = System.getProperty("os.name");
+
+
+    //Mouse
     public static int mX; public static int mY;
 
+
+    //Graphics
     static JFrame window = new JFrame("You can't close me!");
     static int windowWidth = 400; static int windowHeight = 300;
-    static int windowX = 500; static int windowY = 500;
+    static int windowX = 250; static int windowY = 200;
 
     static JButton closeButton = new JButton();
 
 
     public static void main(String[] args) {
-
+        setOperatingSystemName();
 
 
         Timer timer = new Timer();
-        TimerTask setMousePos = new TimerTask() {
+        TimerTask periodic = new TimerTask() {
             @Override
             public void run() {
                 PointerInfo pointerInfo = MouseInfo.getPointerInfo();
                 Point mousePos = pointerInfo.getLocation();
                 mX = (int) mousePos.getX();
                 mY = (int) mousePos.getY();
-                System.out.println(mX + " " + mY);
+//                System.out.println(mX + " " + mY);
+
+                windowX = window.getX();
+                windowY = window.getY();
+//                System.out.println(windowX + " " + windowY);
+                checkWindow();
+
             }
         };
-        timer.schedule(setMousePos,0,100);
+        timer.schedule(periodic,0,50);
 
+        window.addWindowStateListener(new WindowStateListener() {
+            public void windowStateChanged(WindowEvent e) {
+                int newState = e.getNewState();
 
-//        mX = (int) mousePos.getX();
-//        mY = (int) mousePos.getY();
+                if ((newState & Frame.MAXIMIZED_BOTH) != 0 || (e.getNewState() & Frame.ICONIFIED) == Frame.ICONIFIED) {
+                    System.out.println("Edit detected!");
+                    window.setState(Frame.NORMAL);
+                    moveWindow();
+                } else if ((newState & Frame.NORMAL) != 0) {
+                    ;
+                }
+            }
+        });
+
 
 
         window.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
@@ -52,7 +75,7 @@ public class Main extends JFrame{
         window.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
-                System.out.println("test");
+//                System.out.println("test");
                 moveWindow();
             }
         });
@@ -76,16 +99,7 @@ public class Main extends JFrame{
         window.setVisible(true);
 
         while (true){
-//            mX = (int) mousePos.getX();
-//            mY = (int) mousePos.getY();
-//            System.out.println(mX);
-//            mX = (int) mousePos.getX();
-//            mY = (int) mousePos.getY();
-//
-//            if (mX >= windowX && mX <= windowX + 80){
-//                System.out.println("aaaaaa");
-//                System.exit(0);
-//            }
+
         }
     }
 
@@ -101,7 +115,20 @@ public class Main extends JFrame{
         windowY = window.getY();
     }
 
+    public static void setOperatingSystemName(){
+        if (operatingSystem.toLowerCase().contains("windows")){
+            operatingSystem = "windows";
+        }
+        else if (operatingSystem.toLowerCase().contains("mac")){
+            operatingSystem = "mac";
+        }
+        else{
+            operatingSystem = "other";
+        }
+    }
 
-
+    public static void checkWindow(){
+        window.setBounds(windowX,windowY,windowWidth,windowHeight);
+    }
 
 }
